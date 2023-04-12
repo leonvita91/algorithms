0ba136afbb874f8d069904a706d4ba56aff42860
leonvita91 <leonvita91@gmail.com>
2023-04-12 17:11:26 +0100

    add training py file with questions and steps

---
+++ training.py
@@ -0,0 +1,14 @@
+
+# Question 1
+# make a function that do linear search with list
+# the steps
+# do function user two parameters
+# loop through and count from beginning to the end of your list
+# return the index and return None
+# do function to verfiy and pass one parameter
+# check if this parameter didn't return None
+# else print index not found
+# do your list
+# put the first function in a variable and pass the two parameters
+# user the verfiy function and pass your variable that your have been create
+
